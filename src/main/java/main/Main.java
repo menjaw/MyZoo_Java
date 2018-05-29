a7406ff60aa00b74b33826af44dad14bfcb2c95b
menjaw <menjaholm@gmail.com>
2018-05-29 23:31:21 +0200

    add more functionality to example

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,12 @@
  */
 package main;
 
+import decoratorPattern.Blue;
+import decoratorPattern.Brown;
+import decoratorPattern.Feather;
+import decoratorPattern.Fur;
+import interfaces.I_Look;
+import strategyPattern.Fast;
 import strategyPattern.Swim;
 
 /**
@@ -14,20 +20,42 @@ import strategyPattern.Swim;
 public class Main {
 
     public static void main(String[] args) {
+        //Variables
+        I_Look decorate;
 
-        //Elephant number 1
-        Elephant elly = new Elephant("Elly the Elephant", 900, "Wiiopppttioopp trutpp", "Influenza", "Chocolate, Peanuts, Fruit and The");
+        //ELEPHANT NUMBER 1
+        Elephant elly = new Elephant("Elly the Elephant", 900, "Wiiopppttioopp trutpp", "influenza", "chocolate, peanuts, fruit and chamomile tea");
         System.out.println(elly);
 
         //Check the behavior
         System.out.println("Ellys behavior rigth now: " + elly.behavior());
-        
+
+        //Check the look 
+        decorate = new Feather(new Blue(elly));
+        System.out.println("Elly think she has " + decorate.getDescription());
+
         //Check for illness and medicin
-        System.out.println("Ellys illness is: " + elly.getIllness());
+        System.out.println("Elly is sick and migth have " + elly.getIllness());
+
+        //Check for the tempo
+        System.out.println("Ellys tempo: " + elly.tempo());
+
+        //Check for medicin
+        System.out.println("Elly will need " + elly.getMedicin());
+        
+        
+        //Change the look
+        decorate = new Fur(new Brown(elly));
+        System.out.println("Now Elly think she has " + decorate.getDescription());
+        
+        //Change tempo
+        elly.setTempo(new Fast());
+        System.out.println("The tempo has changed and Elly now says " + elly.tempo());
         
         //Change the behavior
         elly.setBehavior(new Swim());
         System.out.println("Elly now thinks: " + elly.behavior());
 
+        
     }
 }
